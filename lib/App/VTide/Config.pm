2b3e9198a73df5425e2e839ddcafbb866b76e699
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-02-15 12:27:26 +1100

    Fixed documentation for config

--- lib/App/VTide/Config.pm
+++ lib/App/VTide/Config.pm
@@ -82,93 +82,72 @@ __END__
 
 =head1 NAME
 
-App::VTide::Config - <One-line description of module's purpose>
+App::VTide::Config - Manage configuration for VTide
 
 =head1 VERSION
 
 This documentation refers to App::VTide::Config version 0.0.1
 
-
 =head1 SYNOPSIS
 
    use App::VTide::Config;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+   my $config = App::VTide::Config->new(
+        global_config => "$ENV{HOME}/.vtide/defaults.yml",
+        local_config  => './.vtide.yml',
+   );
 
+   $config->get();
+   # returns the merged global and local configurations (will always be up
+   # to date with files on disk i.e. files are checked for changes on each
+   # call)
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+This module gets the global and local configuration data for a project.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head2 C<get ()>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+Get the merged local and global configuration files
 
-Name the section accordingly.
+=head2 C<changed ()>
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
+Returns true if either the C<global_config> or C<local_config> files have
+changed since the last read.
 
+=head1 ATTRIBUTES
 
-=head3 C<new ( $search, )>
+=head2 global_config
 
-Param: C<$search> - type (detail) - description
+The name of the global configuration file (Defaults to ~/.vtide/defaults.yml)
 
-Return: App::VTide::Config -
+=head2 local_config
 
-Description:
+The name of the current project's configuration file (Defaults to ./.vtide.yml)
 
-=cut
+=head2 global_time
 
+Last modified time for the C<global_config> file (Defaults to 0)
+
+=head2 local_time
+
+Last modified time for the C<local_config> file (Defaults to 0)
+
+=head2 data
+
+Cached combined global/local data
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
