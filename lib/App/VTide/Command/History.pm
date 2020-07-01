0317813883f5ee46c0c653a7368b1187e5b0a9ea
Ivan Wills <Ivan.Wills2@optus.com.au>
2020-07-02 08:30:03 +1000

    Updating for release of version 0.1.13

--- lib/App/VTide/Command/History.pm
+++ lib/App/VTide/Command/History.pm
@@ -15,7 +15,7 @@ use YAML::Syck;
 
 extends 'App::VTide::Command::Run';
 
-our $VERSION = version->new('0.1.12');
+our $VERSION = version->new('0.1.13');
 our $NAME    = 'history';
 our $OPTIONS = [
     'number|n=i',
@@ -76,7 +76,7 @@ App::VTide::Command::History - Tells you about the terminal you are in
 
 =head1 VERSION
 
-This documentation refers to App::VTide::Command::History version 0.1.12
+This documentation refers to App::VTide::Command::History version 0.1.13
 
 =head1 SYNOPSIS
 
